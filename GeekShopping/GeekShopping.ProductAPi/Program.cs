06a73e2f9473a3c8fcedb7f29480ba275b478b6d
guilherme.santos <guilherme.santos@toshyro.com.br>
2024-09-02 17:04:30 -0300

    migrate

--- GeekShopping/GeekShopping.ProductAPi/Program.cs
+++ GeekShopping/GeekShopping.ProductAPi/Program.cs
@@ -1,3 +1,7 @@
+using GeekShopping.ProductAPi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 namespace GeekShopping.ProductAPi;
 
 public class Program
@@ -5,14 +9,18 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        var connection = builder.Configuration.GetConnectionString("MySQLConnection:MySQLConnection");
+
+        builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 28))));
 
         // Add services to the container.
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
+        builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPi", Version = "v1" }));
+        
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
