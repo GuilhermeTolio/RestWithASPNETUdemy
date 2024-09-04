7c9ee8b88e4dfd44afebc70f3a860929cf69015c
guilherme.santos <guilherme.santos@toshyro.com.br>
2024-09-04 17:36:11 -0300

    Adição do DbContext ao Identity Server

---
+++ GeekShopping/GeekShopping.IdentityServer/Model/Context/MySqlContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model.Context;
+
+public class MySqlContext : IdentityDbContext<ApplicationUser>
+{
+
+    public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+    {
+    }
+
+}
