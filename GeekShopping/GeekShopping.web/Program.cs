ff54174141cef2f68b75f7014617beebca783568
guilherme.santos <guilherme.santos@toshyro.com.br>
2024-09-05 16:13:57 -0300

    Adicionando às Configurações de Segurança ao Startup do GeekShopping.Web

--- GeekShopping/GeekShopping.web/Program.cs
+++ GeekShopping/GeekShopping.web/Program.cs
@@ -1,5 +1,7 @@
 using GeekShopping.web.Services;
 using GeekShopping.web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,26 @@ builder.Services.AddHttpClient<IProductService, ProductService>(
 );
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = "cookies";
+        options.DefaultChallengeScheme = "oidc";
+    }).AddCookie("cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.ClientId = "geek_shopping";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+        options.ClaimActions.MapJsonKey("role", "role", "role");
+        options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
+        options.Scope.Add("geek_shopping");
+        options.SaveTokens = true;
+    });
 
 var app = builder.Build();
 
@@ -25,6 +47,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
